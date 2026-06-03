381a003d4d534dc118ca45a6d87377d626bf3ef9
Яблочко <aminevr102@gmail.com>
2026-06-03 17:53:14 +0500

    commit v0.3

--- web/main/models.py
+++ web/main/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField("Заголовок", max_length=255)
     content = models.TextField("Текст статьи")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
