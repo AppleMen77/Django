4d6fba847ec19ec19a3a6fa3d3cb218129d09782
Яблочко <aminevr102@gmail.com>
2026-06-11 00:01:32 +0500

    Commit
    add ?

--- web/main/admin.py
+++ web/main/admin.py
@@ -1,7 +1,98 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Post, Comment, Profile, Category
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Profile)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Comment)
+# admin.site.register(Profile)
+# admin.site.register(Category)
+
+class CommentFilther(admin.SimpleListFilter):
+    title = "Длина комментария"
+
+    def lakusapsus(self, request, model_admin):
+        return (
+            ("short", "Короткие")
+            ("medium", "Средние")
+            ("long", "Длинные")
+        )
+    def queryset(self, request, queryset):
+        if self.value() == 'short':
+            pass
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at", "category", "views_count",)
+    list_display_links = ("title",)
+    list_editable = ('category',)
+    search_fields = ('title',)
+    list_per_page = 50
+    list_max_show_all = 500
+    date_hierarchy = "created_at"
+
+
+    fieldsets = (
+        ("Основное",{
+            'fields': ('title', 'content', 'author',)
+        }),
+        ("Дополнительно", {
+            'fields': ('views_count', 'category',)
+        })
+    )
+    actions = ["zero_views"]
+
+    def zero_views(self, request, queryset):
+        queryset.update(viwes_count=0)
+
+    zero_views.short_description = "Обнулить просмотры"
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at',)
+    list_filter = ("created_at",)
+    list_per_page = 50
+    date_hierarchy = "created_at"
+
+
+    fieldsets = (
+        ("Основное",{
+            'fields': ('post', 'author',)
+        }),
+        ("Дополнительно", {
+            'fields': ('text',)
+        })
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "bio",)
+    list_display_links = ("name",)
+    list_per_page = 50
+
+    fieldsets = (
+        ("Основное",{
+            'fields': ('owner', 'name',)
+        }),
+        ("Дополнительно", {
+            'fields': ('bio',)
+        })
+    )
+    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "post_count")
+    search_fields = ("name",)
+
+    def post_count(self, object):
+        return object.posts.count
+    post_count.short.decription = "Количество постов"
+    post_count.admin_order_field = "post_count"
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(post_count = Count('posts'))
+        return queryset
--- web/main/models.py
+++ web/main/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField("Название", max_length=30)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField("Заголовок", max_length=255)
     content = models.TextField("Текст статьи")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey("Категории", null=True, related_name='posts' )
+    category = models.ForeignKey(Category, null=True, related_name='posts', on_delete=models.CASCADE )
     views_count = models.IntegerField(default=0)
 
     class Meta:
@@ -36,7 +45,7 @@ class Comment(models.Model):
 
 class Profile(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
-    name = models.CharField("Имя", max_length=20)
+    name = models.CharField("Имя", max_length=20, default="Anonymous")
     avatar = models.ImageField("Аватар", blank=True)
     bio = models.TextField("О себе", max_length=100, blank=True)
 
@@ -46,14 +55,4 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Профиль {self.name}"
-    
-class Category(models.Model):
-    name = models.CharField("Название", max_length=30)
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
-
-    def __str__(self):
-        return self.name
     
